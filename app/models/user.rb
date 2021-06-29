0e92d9cbe2059082c6475debd2899e7352d4c20c
niboshi3 <nib0shi.vs.nek0n0c0@gmail.com>
2021-06-29 22:39:05 +0900

    テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # 必須項目：nickname、email、password、family_name、family_name_kana、first_name、first_name_kana、birthday     
   validates :nickname,               presence: true
-  validates :email,     presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } # メールアドレスの正規表現
-  validates :encrypted_password,     presence: true, length: { minimum: 7 } # 文字数の正規表現
-  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
-  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
-  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
-  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
+  validates :email,     presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } # メールアドレスの正規表現 (メールアドレスは一意性であり、@を含む)
+  validates :encrypted_password,     presence: true, length: { minimum: 7 } # 文字数の正規表現 (パスワードは、6文字以上で半角英数字混合での入力が必須。パスワードとパスワード（確認用）は、値の一致が必須)
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現 (全角（漢字・ひらがな・カタカナ）で入力)
+  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現(全角（カタカナ）で入力)
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現（名字と一緒）
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現（名字と一緒）
   validates :birthday,               presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    family_name {"山田"}
+    family_name_kana {"ヤマダ"}
+    first_name {"太郎"}
+    first_name_kana {"タロウ"}
+    birthday {Faker::Date.birthday}
+  end
+end
