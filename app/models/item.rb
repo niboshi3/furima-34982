ba58f2b05c8c67254b313bf81ca51085f23272c7
niboshi3 <nib0shi.vs.nek0n0c0@gmail.com>
2021-07-05 21:56:59 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  
+  with_options presence: true do 
+    validates :image
+    validates :item_name
+    validates :appeal_point
+    
+    #選択肢「--」は保存できないようにする
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :shipping_charge_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :shipping_date_id, numericality: { other_than: 1 }
+    #半角数字、¥300-¥9,999,999間のみ保存可能
+    validates :price,format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
 end
