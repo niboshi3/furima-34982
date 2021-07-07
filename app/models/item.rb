5725b4fbe80fcfa4ff28c1117850a385ea87b31a
niboshi3 <nib0shi.vs.nek0n0c0@gmail.com>
2021-07-07 16:44:39 +0900

    可読性向上のため記述まとめ

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,11 +14,13 @@ class Item < ApplicationRecord
     validates :appeal_point
     
     #選択肢「--」は保存できないようにする
-    validates :category_id, numericality: { other_than: 1 }
-    validates :status_id, numericality: { other_than: 1 }
-    validates :shipping_charge_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :shipping_date_id, numericality: { other_than: 1 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :shipping_date_id
+    end
     #半角数字、¥300-¥9,999,999間のみ保存可能
     validates :price,format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
