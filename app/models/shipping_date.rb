234f2b1914209127a758c9bdf4b0d78b7a79bdd0
niboshi3 <nib0shi.vs.nek0n0c0@gmail.com>
2021-07-10 13:09:03 +0900

    商品詳細表示画面の実装

--- app/models/shipping_date.rb
+++ app/models/shipping_date.rb
@@ -1,6 +1,6 @@
 class ShippingDate < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },  { id: 2, name: '１〜２で発送' },  { id: 3, name: '２〜３で発送' },  { id: 4, name: '４〜７で発送' }
+    { id: 1, name: '---' },  { id: 2, name: '１〜２日で発送' },  { id: 3, name: '２〜３日で発送' },  { id: 4, name: '４〜７日で発送' }
   ]
 
   include ActiveHash::Associations
