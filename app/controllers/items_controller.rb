47d663142c08b4149d97b4ef4ee23a1eba61712a
niboshi3 <nib0shi.vs.nek0n0c0@gmail.com>
2021-07-11 20:38:54 +0900

    不要な記述の削除とbeforeの順序入れ替え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :set_security, only: [:edit, :update]
-  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @items = Item.order("created_at DESC")
@@ -46,7 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def set_security
-    unless user_signed_in? && current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
       redirect_to action: :index
     end
   end
